47ac1527594bcd8b52b1844304280d58fa59624a
Kelvin Stephano <kstephano@gmail.com>
2021-12-15 17:49:04 +0000

    dbInit changes

--- dbInit.js
+++ dbInit.js
@@ -18,31 +18,35 @@ async function initDB() {
     return client.db(dbName);
 }
 
-const db = await initDB(); 
+async function init() {
+    const db = await initDB(); 
 
-db.users.drop();
-db.habits.drop();
+    db.users.drop();
+    db.habits.drop();
 
-db.users.insertOne(
-    { 
-        userEmail: "initialUser@email.com",
-        passwordDigest: "password",
-        refreshTokens: [],
-        userName: "Initial User",
-    },
-);
+    db.users.insertOne(
+        { 
+            userEmail: "initialUser@email.com",
+            passwordDigest: "password",
+            refreshTokens: [],
+            userName: "Initial User",
+        },
+    );
 
-db.habits.insertOne(
-    {
-        userEmail: "initialUser@email.com",
-        userName: "Initial User",
-        habitName: "Water",
-        frequency: 1,
-        unit: "cups",
-        expectedAmount: 3,
-        currentAmount: 0,
-        topStreak: 5,
-        currentStreak: 3,
-        lastLog: "2021-12-11T11:31:21.988Z"
-    }
-);
+    db.habits.insertOne(
+        {
+            userEmail: "initialUser@email.com",
+            userName: "Initial User",
+            habitName: "Water",
+            frequency: 1,
+            unit: "cups",
+            expectedAmount: 3,
+            currentAmount: 0,
+            topStreak: 5,
+            currentStreak: 3,
+            lastLog: "2021-12-11T11:31:21.988Z"
+        }
+    );
+}
+
+init();
