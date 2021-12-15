b95143e99e8a5144c25f19eb04d501af916ae42e
Kelvin Stephano <kstephano@gmail.com>
2021-12-15 17:46:26 +0000

    dbInit changes

--- dbInit.js
+++ dbInit.js
@@ -1,4 +1,20 @@
-const { initDB } = require('../api/dbConfig');
+const { MongoClient } = require('mongodb');
+
+const dbName = "habit-tracker-db"; // db name
+const uri = "mongodb+srv://AATTWM:<password>@cluster0.kxhwc.mongodb.net/myFirstDatabase?";
+
+async function initConnection() {
+    // Create a new MongoClient
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    // Connect the client to the server
+    await client.connect();
+
+    return client;
+}
+
+const client = await initConnection();
+console.log(`connected to database ${dbName}`);
+const db = client.db(dbName);
 
 const db = initDB(); 
 
